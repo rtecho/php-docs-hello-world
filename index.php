1f2b8790c4fc3373160edce9b4ac8088e3101548
Ratsakon Techo <116138495+rtecho@users.noreply.github.com>
2022-12-10 01:25:39 +0700

    update echo header 2

--- index.php
+++ index.php
@@ -1,15 +1,7 @@
 <?php
 
 echo "Hello World!";
-if (!function_exists('getallheaders')){ 
-    function getallheaders() { 
-       $headers = ''; 
-       foreach ($_SERVER as $name => $value) { 
-            if (substr($name, 0, 5) == 'HTTP_') { 
-                $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value; 
-            } 
-       } 
-       return $headers; 
-    } 
+foreach (getallheaders() as $name => $value) {
+    echo "$name: $value\n";
 }
 ?>
