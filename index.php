404338036ad5e06a3cf9832168dcb088d063d887
Ratsakon Techo <116138495+rtecho@users.noreply.github.com>
2022-12-10 01:37:56 +0700

    update echo header 4

--- index.php
+++ index.php
@@ -1,7 +1,4 @@
 <?php
-foreach (getallheaders() as $name => $value) {
-    echo "$name: $value\n";
-}
 $headers =  getallheaders();
 foreach($headers as $key=>$val){
   echo $key . ': ' . $val . '<br>';
