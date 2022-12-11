a7c0bcc8d7b1bf4cf61b15b4de631595e5022c61
Ratsakon Techo <116138495+rtecho@users.noreply.github.com>
2022-12-11 23:21:51 +0700

    update echo debug

--- index.php
+++ index.php
@@ -1,6 +1,28 @@
-<?php
-$headers =  getallheaders();
-foreach($headers as $key=>$val){
-  echo $key . ': ' . $val . '<br>';
-}
-?>
+<html>
+
+<head>
+  <title>Debug page</title>
+</head>
+
+<body style="background-color: #00E60A;">
+  <?php
+    $requestPageUrl = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"];
+    $pubIp = file_get_contents('https://api.ipify.org');
+  ?>
+  <h3>Debug information</h3>
+  <ul>
+    <li><strong>Request Url</strong><span>: <?php echo $requestPageUrl; ?></span></li>
+    <li><strong>Outbound public IP</strong><span>: <?php echo $pubIp ?></span></li>
+    <li><strong>Inbound client IP</strong><span>: <?php echo $_SERVER['REMOTE_ADDR']; ?></span></li>
+  </ul>
+  <strong>Headers</strong>
+  <ul>
+    <?php
+      foreach (getallheaders() as $name => $value) {
+        echo "<li><strong>$name</strong><span>: $value</span></li>";
+      }
+    ?>
+  </ul>
+</body>
+
+</html>
